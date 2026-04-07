3bea0254aad4cbcd780b75780665676569bbf7e7
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2026-04-07 12:11:11 -0400

    added code to create QC for reviewal of lesion pred seg

--- detection/predict_lesion_seg.py
+++ detection/predict_lesion_seg.py
@@ -8,6 +8,7 @@ Input:
     -d: path to the dataset (BIDS format)
     -o: path to the output folder where the lesion segmentation will be saved
     -cerv: flag to only perform the segmentation on cervical scans (i.e. those that contain "cerv" in their name)
+    -label: flag to label each lesion with a different label
 
 Author: Pierre-Louis Benveniste
 """
@@ -15,13 +16,17 @@ import os
 from pathlib import Path
 import argparse
 from tqdm import tqdm
+import nibabel as nib
+import numpy as np
+from scipy.ndimage import label
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Perform lesion segmentation on all T2w axial scans of the dataset which will be used for critical lesion detection.")
     parser.add_argument("-d", "--dataset_path", type=str, required=True, help="Path to the dataset (BIDS format).")
-    parser.add_argument("-o", "--output_folder", type=str, required=True, help="Path to the output folder where the lesion segmentation will be saved.")
+    parser.add_argument("-o", "--output_folder", type=str, required=True, help="Path to the output folder where the lesion segmentation will be saved. (It can be the derivatives folder)")
     parser.add_argument("-cerv", "--cervical", action="store_true", help="Flag to only perform the segmentation on cervical scans.")
+    parser.add_argument("-label", "--label_lesions", action="store_true", help="Flag to label each lesion with a different label. By default, all lesions will have the same label (i.e. 1).")
     return parser.parse_args()
 
 
@@ -75,16 +80,30 @@ def main(dataset_path, output_folder, cervical=False):
     for scan in tqdm(t2w_axial_scans):
         sub_name = scan.split("/")[-1].split("_")[0]
         ses_name = scan.split("/")[-1].split("_")[1]
+        # Copy the relatie path of the scan in the output folder
+        relative_path = Path(os.path.relpath(scan, dataset_path)).parent
+        output_pred_folder = os.path.join(output_folder, relative_path)
         # Build an output folder:
-        output_folder_scan = os.path.join(output_folder, sub_name, ses_name)
-        os.makedirs(output_folder_scan, exist_ok=True)
+        os.makedirs(output_pred_folder, exist_ok=True)
         # Build output path:
-        sc_mask = os.path.join(output_folder_scan, scan.split("/")[-1].replace(".nii.gz", "_label-SC_seg.nii.gz"))
-        lesion_mask = os.path.join(output_folder_scan, scan.split("/")[-1].replace(".nii.gz", "_label-lesion_seg.nii.gz"))
+        sc_mask = os.path.join(output_pred_folder, scan.split("/")[-1].replace(".nii.gz", "_label-SC_seg.nii.gz"))
+        lesion_mask = os.path.join(output_pred_folder, scan.split("/")[-1].replace(".nii.gz", "_label-lesion_seg.nii.gz"))
         # Run the SC seg command
         assert os.system(f"SCT_USE_GPU=1 sct_deepseg spinalcord -i {scan} -o {sc_mask}") == 0, "Error running the SC segmentation model"
         # Run the lesion seg command
         assert os.system(f"SCT_USE_GPU=1 sct_deepseg lesion_ms -i {scan} -o {lesion_mask} -qc {qc_folder} -qc-seg {sc_mask} -qc-plane Axial" ) == 0, "Error running the lesion segmentation model"
+        # If the label_lesions flag is set, we will label each lesion with a different label (i.e. 1, 2, 3, etc.)
+        if args.label_lesions:
+            # We load the lesion mask and we label each connected component with a different label
+            lesion_mask_nii = nib.load(lesion_mask)
+            lesion_mask_data = lesion_mask_nii.get_fdata()
+            # We label each connected component with a different label
+            labeled_lesions, num_lesions = label(lesion_mask_data)
+            # We save the new lesion mask
+            labeled_lesions_nii = nib.Nifti1Image(labeled_lesions, lesion_mask_nii.affine, lesion_mask_nii.header)
+            # Output path for labeled seg
+            labeled_lesion_mask = os.path.join(output_pred_folder, scan.split("/")[-1].replace(".nii.gz", "_label-lesion_seg-labeled.nii.gz"))
+            nib.save(labeled_lesions_nii, labeled_lesion_mask)
 
 
 if __name__ == "__main__":
